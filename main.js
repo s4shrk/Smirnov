e7210b536dd9ed8bc5cd6335a61252951e33e516
Smirnov <smirnov1play@gmail.com>
2025-04-24 16:43:35 +0300

    Високосный год

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ if (number % 3 === 0 && number % 5 === 0) {
 } else {
     console.log(number + ' => Не Fizz и не Buzz');
 }
+
+
+
+const year = Math.floor(Math.random() * ((2120 - 1980) / 10 + 1)) * 10 + 1980;
+let isLeap;
+if (year % 4 !== 0) {
+    isLeap = false;
+} else if (year % 100 !== 0) {
+    isLeap = true;
+} else if (year % 400 === 0) {
+    isLeap = true;
+} else {
+    isLeap = false;
+}
+console.log(year + (isLeap ? ' — високосный год' : ' — не високосный год'));
