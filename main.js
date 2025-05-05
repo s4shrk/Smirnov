cc1186161c93bcb91c4bc8a2b054d48032823e23
Smirnov <smirnov1play@gmail.com>
2025-05-05 15:28:33 +0300

    свойства и методы. шаблонные строки

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
-const number = Math.floor(Math.random() * (15 - 3 + 1)) + 3;
-if (number % 3 === 0 && number % 5 === 0) {
-    console.log(number + ' => FizzBuzz');
-} else if (number % 3 === 0) {
-    console.log(number + ' => Fizz');
-} else if (number % 5 === 0) {
-    console.log(number + ' => Buzz');
-} else {
-    console.log(number + ' => Не Fizz и не Buzz');
-}
+// длина строки
+const s4sha = "пример";
+console.log(s4sha.length); // длина строки
+console.log(s4sha.toUpperCase()); // строка в заглавном
+
+
+// первый и последний символ строки
+const slovo = "example";
+const firstChar = slovo.charAt(0).toUpperCase();
+const lastChar = slovo.charAt(slovo.length - 1).toUpperCase();
+console.log(firstChar + slovo.slice(1, slovo.length - 1) + lastChar); // ExamplE
+
+
+// поиск позиции символа
+const milk = "молоко";
+const firstO = milk.indexOf("о");
+const lastO = milk.lastIndexOf("о");
+console.log(`Первая 'о': ${firstO}, Последняя 'о': ${lastO}`);
+
+
+// поиск вхождения символа
+const milks = "молоко";
+const hasU = milks.includes("у");
+const hasM = milks.includes("м");
+console.log(`Есть 'у': ${hasU}, Есть 'м': ${hasM}`);
 
 
 
-const year = Math.floor(Math.random() * ((2120 - 1980) / 10 + 1)) * 10 + 1980;
-let isLeap;
-if (year % 4 !== 0) {
-    isLeap = false;
-} else if (year % 100 !== 0) {
-    isLeap = true;
-} else if (year % 400 === 0) {
-    isLeap = true;
-} else {
-    isLeap = false;
-}
-console.log(year + (isLeap ? ' — високосный год' : ' — не високосный год'));
+// начало строки
+const hello = ["привет", "ветеринар"];
+const startsWithVet = hello.map(word => word.startsWith("вет"));
+console.log(startsWithVet); // [false, true]
+
+
+
+//разделитель
+const text = 'this_is_an_example_string';
+const result = text.split('_');
+console.log(result); // [ 'this', 'is', 'an', 'example', 'string' ]
+
+
+
+// шаблонные строки
+const name = 'Ruslan';
+const message = `
+Добро пожаловать, ${name}, благодарим за регистрацию!
+Надеемся, Вам понравится`;
+console.log(message);
+
+
+
